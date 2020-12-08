91380ff826ce092531dec94d3b3b1c2fdc57bf24
Malwe <menvou.lamwe@mail.ru>
2020-12-08 09:43:25 +0300

    Peerserver

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import logger from './logger';
 import app from './app';
+import { PeerServer } from 'peer';
 
 const port = app.get('port');
 const server = app.listen(port);
+
+PeerServer({ port: 4000, path: '/' });
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
 );
 
 server.on('listening', () =>
-  logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+  logger.info(
+    'Feathers application started on http://%s:%d',
+    app.get('host'),
+    port
+  )
 );
