890bdd6a53294e852b5013000f9d0c13b18f8d0d
Malwe <menvou.lamwe@mail.ru>
2021-06-18 17:41:49 +0300

    add api url on prod

--- src/index.ts
+++ src/index.ts
@@ -13,9 +13,5 @@ process.on('unhandledRejection', (reason, p) =>
 );
 
 server.on('listening', () =>
-  logger.info(
-    'Feathers application started on http://%s:%d',
-    app.get('host'),
-    port
-  )
+  logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
 );
